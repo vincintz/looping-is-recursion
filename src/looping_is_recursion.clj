27f909fb5ecf00fa1a275af14810186155192b18
Theo Reyes <vincintz@yahoo.com>
2015-02-14 22:47:20 +0800

    seq=

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,26 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [acc exp]
+                 (if (<= exp 0) acc
+                     (recur (* acc base) (- exp 1))))]
+    (helper 1 exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [cur xs]
+                 (if (empty? xs) cur
+                     (recur (first xs) (rest xs))))]
+    (helper nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (if (empty? seq1)
+    (empty? seq2)
+    (let [x1  (first seq1)
+          x2  (first seq2)]
+      (and (boolean  x1)
+           (boolean x2)
+           (== x1 x2)
+           (recur (rest seq1) (rest seq2))))))
 
 (defn find-first-index [pred a-seq]
   ":(")
@@ -23,4 +36,3 @@
 
 (defn cut-at-repetition [a-seq]
   [":("])
-
